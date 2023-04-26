69b0f5f8220fc45d435776401f727b20555d4e4a
Marco Laptop <marco1996@hotmail.de>
2023-04-26 15:59:34 +0200

    implemented function for filtering outliers from data by threshold

--- src/Tests/test_build_features.py
+++ src/Tests/test_build_features.py
@@ -6,14 +6,28 @@ To run this file enter into console: py ./src/tests/test_build_features.py
 # Import files and bibs
 from src.features import build_features
 import pandas as pd
+import pandas.testing as pd_test
+import unittest
 
-def test_filter_values_by_threshold():
-    train_df = pd.read_csv('data/raw/train_values.csv')
-    
+class TestBuildFeatures(unittest.TestCase):
+
+    def test_filter_values_by_threshold(self):
+        train_df = pd.read_csv('data/raw/train_values.csv')
+        print(train_df.head())
+        
+        # Count the number of unique values in each row of the 'geo_level_1_id' column
+        #train_df['count'] = train_df['geo_level_1_id'].count()
+
+        # Print the first 10 rows of the DataFrame with the 'count' column
+        #print(train_df[['geo_level_1_id', 'count']].head(10))
+        filter_values = build_features.filter_values_by_threshold(train_df, 0.98)
+        
+        self.assertListEqual(filter_values['count_floors_pre_eq'], [4,5,6,7,8,9])
 
 
-    build_features.filter_values_by_threshold(train_df, -1)
-    
+if __name__ == '__main__':
+    unittest.main()
+
 # Starting the tests
-print("Starting tests for build_features.py")
-test_filter_values_by_threshold()
+#print("Starting tests for build_features.py")
+#test_filter_values_by_threshold()
