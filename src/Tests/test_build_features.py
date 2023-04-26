a594fd54a19248c36532fdc5e6c31e9f451fd238
Marco Laptop <marco1996@hotmail.de>
2023-04-26 11:14:36 +0200

    added description for test file

--- src/Tests/test_build_features.py
+++ src/Tests/test_build_features.py
@@ -1,9 +1,16 @@
+'''
+This is a testing file for testing the src/features/build_features scripts.
+To run this file enter into console: py ./src/tests/test_build_features.py
+'''
+
 # Import files and bibs
 from src.features import build_features
 import pandas as pd
 
 def test_filter_values_by_threshold():
     train_df = pd.read_csv('data/raw/train_values.csv')
+    
+
 
     build_features.filter_values_by_threshold(train_df, -1)
     
