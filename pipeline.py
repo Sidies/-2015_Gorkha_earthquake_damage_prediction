e1c36e41c9cb692929fcc78520acf7a22198712c
Patrick Roth <uyyrj@student.kit.edu>
2023-04-29 21:08:40 +0200

    add check_dataframe_for_imbalanced_features to pipeline

--- pipeline.py
+++ pipeline.py
@@ -112,7 +112,8 @@ def run(extract_test_set=False):
         y_test = y_test.astype('category')
         
     # find columns that can be dropped (uninformative columns / highly correlated columns)
-    columns_to_remove = [] #TODO
+    columns_to_remove = build_features.check_dataframe_for_imbalanced_features(X_train, 0.9)
+    
 
 
     # ============================================= #
